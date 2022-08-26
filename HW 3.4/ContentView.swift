d88522d3ea0fa682c211a7987d3adb353ab2f9ff
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-08-26 22:43:42 +0300

    Initial Commit

---
+++ HW 3.4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW 3.4
+//
+//  Created by Александр on 26.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW 3.4/HW_3_4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3_4App.swift
+//  HW 3.4
+//
+//  Created by Александр on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_3_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
